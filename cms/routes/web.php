f05582fb3a14a6fed625694584700cb0977cb823
takanov <t.taka.creative@gmail.com>
2023-08-30 08:54:49 +0900

    update

---
+++ cms/routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Models\Book;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//本：ダッシュボード表示(books.blade.php)
+Route::get('/', [BookController::class,'index'])->middleware(['auth'])->name('book_index');
+Route::get('/dashboard', [BookController::class,'index'])->middleware(['auth'])->name('dashboard');
+
+//本：追加 
+Route::post('/books',[BookController::class,"store"])->name('book_store');
+
+//本：削除 
+Route::delete('/book/{book}', [BookController::class,"destroy"])->name('book_destroy');
+
+//本：更新画面
+Route::post('/booksedit/{book}',[BookController::class,"edit"])->name('book_edit'); //通常
+Route::get('/booksedit/{book}', [BookController::class,"edit"])->name('edit');      //Validationエラーありの場合
+
+//本：更新画面
+Route::post('/books/update',[BookController::class,"update"])->name('book_update');
+
+
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
